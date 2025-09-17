46af154e1012ca33ac3d3e75e06627125b4f36ac
Zephilinox <Zephilinox@hotmail.co.uk>
2025-09-17 23:45:44 +0100

    fix formatting

--- src/game/battle/battle_manager.gd
+++ src/game/battle/battle_manager.gd
@@ -4,9 +4,11 @@ const PLAYER = preload("res://game/characters/player/player.tscn")
 
 @onready var player_character := PLAYER.instantiate()
 
+
 func _ready() -> void:
 	add_child(player_character)
 	print(player_character.stats.attack)
+
 
 func handle_basic_attack(attacker: Node, defender: Node) -> void:
 	var damage: int = attacker.stats.attack - defender.stats.defense
--- src/game/main.gd
+++ src/game/main.gd
@@ -2,9 +2,11 @@ extends Node
 
 const BATTLE = preload("res://game/battle/battle.tscn")
 
+
 func _ready() -> void:
 	var battle = BATTLE.instantiate()
 	add_child(battle)
+
 
 func _process(_delta: float) -> void:
 	pass
