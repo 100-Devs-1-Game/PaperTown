19c8cde6e308bbbde80d705f743c0df5a209d17a
Zephilinox <Zephilinox@hotmail.co.uk>
2025-09-18 01:06:14 +0100

    quick dialogue test

---
+++ src/game/dialogue/test_dialogue_01.gd
@@ -0,0 +1,15 @@
+extends Node3D
+
+const BALLOON := preload("res://game/dialogue/balloon.tscn")
+const DIALOGUE := preload("res://game/dialogue/test_dialogue_01.dialogue")
+
+var super_cool_test_var: bool = false
+
+
+func _ready() -> void:
+	#DialogueManager.show_example_dialogue_balloon(DIALOGUE)
+	DialogueManager.show_dialogue_balloon_scene(BALLOON, DIALOGUE, "start", [self])
+
+
+func _process(_delta: float) -> void:
+	pass
