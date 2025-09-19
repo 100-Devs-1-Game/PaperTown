d0a508baa0c07fed782c63998d9b66bc91308aba
Zephilinox <Zephilinox@hotmail.co.uk>
2025-09-19 19:45:35 +0100

    Change the floating text wobble

--- src/game/other/floating_text/tilt_effect.gd
+++ src/game/other/floating_text/tilt_effect.gd
@@ -8,6 +8,8 @@ extends RichTextEffect
 # - Use [tilt]hello[/tilt_effect] in text.
 var bbcode := "tilt"
 
+var a := 0
+
 
 func _process_custom_fx(char_fx: CharFXTransform) -> bool:
 	# I only vaguely understood what's going on here
@@ -17,14 +19,19 @@ func _process_custom_fx(char_fx: CharFXTransform) -> bool:
 
 	var ts := TextServerManager.get_primary_interface()
 	var glyph_size := ts.font_get_glyph_size(char_fx.font, Vector2i(160, 0), char_fx.glyph_index)
-	var pivot := Vector2(glyph_size.x * 0.5, 0.0)
-	var angle := 0.1 * sin(char_fx.elapsed_time * 3.0 + float(char_fx.relative_index))
+	var _pivot := Vector2(glyph_size.x * 0.5, glyph_size.y * 0.5)
+	var angle := 0.1 * sin(char_fx.elapsed_time * 6.0 + float(char_fx.relative_index))
 
 	var xf := char_fx.transform
-	xf = xf * Transform2D(0, pivot / 2.0)
-	xf = xf * Transform2D(angle, Vector2.ZERO)
-	xf = xf * Transform2D(0, -pivot / 2.0)
+	#todo: the pivits cause it to stutter/jitter
+	var xf2 = xf  # * Transform2D(0, pivot)
+	var xf3 = xf2 * Transform2D(angle, Vector2.ZERO)
+	var xf4 = xf3  # * Transform2D(0, -pivot)
+	var xf5 = xf4
+	#xf5.origin.x = floor(xf4.origin.x)
+	#xf5.origin.y = floor(xf4.origin.y)
+	#print("\nxf1 %s\nxf2 %s\nxf3 %s\nxf4 %s\nxf5 %s\n" % [xf, xf2, xf3, xf4, xf5])
 
-	char_fx.transform = xf
+	char_fx.transform = xf5
 
 	return true
