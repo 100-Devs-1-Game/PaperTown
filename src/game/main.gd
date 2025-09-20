9c438b45e075677f8a417b8ed468ad3ea88f3116
Zephilinox <Zephilinox@hotmail.co.uk>
2025-09-21 00:46:39 +0100

    Remove battle from main to stop player spawning

--- src/game/main.gd
+++ src/game/main.gd
@@ -1,11 +1,8 @@
 extends Node
 
-const BATTLE = preload("res://game/battle/battle.tscn")
-
 
 func _ready() -> void:
-	var battle = BATTLE.instantiate()
-	add_child(battle)
+	pass
 
 
 func _process(_delta: float) -> void:
