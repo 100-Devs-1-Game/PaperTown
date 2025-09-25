a72ad3ea93595d7df72686dda1e51dc8b28bde1e
Zephilinox <Zephilinox@hotmail.co.uk>
2025-09-25 19:06:08 +0100

    update player and boar visuals (mostly collision shape + blob shadow to stop it from feeling floaty)

--- src/game/characters/components/blob_shadow.gd
+++ src/game/characters/components/blob_shadow.gd
@@ -4,15 +4,23 @@ extends MeshInstance3D
 @export var raycast_distance: float = 5.0
 
 var default_scale: Vector3
+var default_offset: Vector3
 
 
 func _ready() -> void:
 	default_scale = scale
+	default_offset = position
 
 
 func _physics_process(_delta: float):
-	var start := (get_parent() as Node3D).global_transform.origin + Vector3.UP * 0.1
-	var end := (get_parent() as Node3D).global_transform.origin + Vector3.DOWN * raycast_distance
+	var start := (
+		(get_parent() as Node3D).global_transform.origin + (Vector3.UP * 0.1) + default_offset
+	)
+	var end := (
+		(get_parent() as Node3D).global_transform.origin
+		+ (Vector3.DOWN * raycast_distance)
+		+ default_offset
+	)
 	var space_state := get_world_3d().direct_space_state
 	var rayparams := PhysicsRayQueryParameters3D.new()
 	rayparams.from = start
@@ -24,8 +32,8 @@ func _physics_process(_delta: float):
 		var hit_position: Vector3 = result.position
 		var hit_normal: Vector3 = result.normal
 		global_transform.origin = hit_position + hit_normal * 0.01
-		transform.origin.x = 0
-		transform.origin.z = 0
+		transform.origin.x = default_offset.x
+		transform.origin.z = default_offset.z
 
 		look_at(hit_position + hit_normal, Vector3.LEFT)
 		rotation.x = 0
