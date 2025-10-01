454a949764290763fbd39d3115151264ba9d3e13
BigP4nda <therealbigp4nda@gmail.com>
2025-10-01 10:04:45 -0500

    Refactor QTE indicator logic and update asset imports
    Fixed QTE indicator to properly check for success and appear and disappear at the necessary times.

--- src/game/battle/qte_indicator.gd
+++ src/game/battle/qte_indicator.gd
@@ -25,31 +25,8 @@ func _process(delta: float) -> void:
 		var shrink_rate = .7 / timer.wait_time
 		qte_ring.scale.x -= shrink_rate * delta
 		qte_ring.scale.y -= shrink_rate * delta
-		if qte_ring.scale.x <= 0.01 or qte_ring.scale.y <= 0.01:
-			qte_result.emit(false)
-			is_active = false
-			timer.stop()
-			return
-
-
-func start_qte() -> void:
-	show()
-	is_active = true
-	qte_ring.scale = initial_ring_scale
-	timer.start()
-
-
-func check_for_success():
-	print("Current Ring Size: %s" % current_ring_scale)
-	print("Sweet Spot Size: %s" % sweet_spot.scale)
-	if current_ring_scale <= sweet_spot.scale:
-		qte_result.emit(true)
-		print(qte_result)
-		is_active = false
-	else:
-		qte_result.emit(false)
-		print(qte_result)
-		is_active = false
+	if not is_active:
+		hide()
 
 
 func _on_timer_timeout() -> void:
@@ -61,8 +38,32 @@ func _on_timer_timeout() -> void:
 
 func _on_button_pressed() -> void:
 	if is_active:
-		current_ring_scale = qte_ring.get_scale()
-		if current_ring_scale.length() > sweet_spot.scale.length():
-			qte_ring.scale = current_ring_scale
-			timer.stop()
-			check_for_success()
+		stop_qte()
+		check_for_success()
+
+
+func check_for_success():
+	if current_ring_scale <= sweet_spot.scale:
+		qte_result.emit(true)
+		print(qte_result)
+		is_active = false
+	else:
+		qte_result.emit(false)
+		print(qte_result)
+		is_active = false
+
+
+func start_qte() -> void:
+	show()
+	is_active = true
+	qte_ring.scale = initial_ring_scale
+	timer.start()
+
+
+func stop_qte() -> void:
+	is_active = false
+	print("Current Ring Size: %s" % current_ring_scale)
+	print("Sweet Spot Size: %s" % sweet_spot.scale)
+	current_ring_scale = qte_ring.get_scale()
+	qte_ring.scale = current_ring_scale
+	timer.stop()
