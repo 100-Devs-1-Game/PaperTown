064f7920565d156238bfb00cc3f5a711107903d5
Zephilinox <Zephilinox@hotmail.co.uk>
2025-10-01 11:11:01 +0100

    Add screen transitions + reorganise ui folder a bit

---
+++ src/game/ui/screen_transition.gd
@@ -0,0 +1,4 @@
+class_name ScreenTransition
+extends Control
+
+@onready var animated_sprite_2d: AnimatedSprite2D = %AnimatedSprite2D
--- src/game/ui/victory_animation.gd
+++
@@ -1,4 +0,0 @@
-extends Control
-
-func _ready() -> void:
-	%AnimatedSprite2D.play()
