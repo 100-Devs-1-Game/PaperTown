ba97cdc28580711fc0c4c2052f26f1ad56ed6097
Zephilinox <Zephilinox@hotmail.co.uk>
2025-10-09 14:03:09 +0100

    Add baby boar unlock animation

--- src/autoloads/signals.gd
+++ src/autoloads/signals.gd
@@ -3,3 +3,4 @@ extends Node
 signal battle_started(enemy: Node3D)
 signal battle_lost(enemy: Node3D)
 signal battle_won(enemy: Node3D)
+signal pet_unlocked()
--- src/game/game.gd
+++ src/game/game.gd
@@ -3,11 +3,15 @@ extends Node3D
 
 @onready var everything: Everything = %Everything
 @onready var scale_count: Label = %ScaleCount
+@onready var animation_player: AnimationPlayer = %AnimationPlayer
 
 var overworld_manager: OverworldManager
 
 func _ready() -> void:
 	Audio.play_music(Audio.MUSIC_OVERWORLD)
+	Signals.pet_unlocked.connect(func():
+		animation_player.play(&"pet_unlock")
+	)
 
 	overworld_manager = get_tree().get_first_node_in_group(&"overworld")
 	assert(overworld_manager)
