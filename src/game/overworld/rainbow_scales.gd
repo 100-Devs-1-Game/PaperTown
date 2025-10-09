e22fc7e2daad18ddd2cf1f2ad1d84a4655d89fa1
Zephilinox <Zephilinox@hotmail.co.uk>
2025-10-09 16:27:04 +0100

    Add text popup when collecting scales

--- src/game/overworld/rainbow_scales.gd
+++ src/game/overworld/rainbow_scales.gd
@@ -11,11 +11,16 @@ func _physics_process(delta: float) -> void:
 func on_body_entered(body) -> void:
 	if not body == get_tree().get_first_node_in_group(&"player"):
 		return
-	
+
 	var overworld_manager = get_tree().get_first_node_in_group(&"overworld")
-	
+
 	overworld_manager.add_rainbow_scale()
-	
-	# TODO: VFX
-	
+
+	var ft3d := FloatingText.spawn(Dialogue.player.global_position + Vector3(0, 1, 2), "+1")
+	FloatingText.colour(ft3d, Color.ORANGE)
+	ft3d.scale *= 0.5
+	FloatingText.animate_towards(
+		ft3d, ft3d.global_position + (Vector3(0, 2, 0).normalized() * 2), 2
+	)
+
 	self.queue_free()
